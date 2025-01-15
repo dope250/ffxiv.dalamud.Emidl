2c5f0083d85084dc4d1609b2b1bbbf0d57ec9faa
Dominic <dope250@electronicping.net>
2025-01-15 19:33:43 +0100

    Init

---
+++ Emidl/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Game.ClientState;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using Dalamud.Configuration;
+using System;
+using System.Linq;
+using System.Timers;
+using System.Collections.Generic;
+
+namespace Emidl;
+
+public sealed class Plugin : IDalamudPlugin
+{
+
+}
